1ae80e1af76352d098fc34ab339646dfe130d9f4
Logan <liubin3310@gmail.com>
2022-10-27 10:20:27 +0800

    fix:fix block errors

--- ios/Classes/Module/SignalingManager.swift
+++ ios/Classes/Module/SignalingManager.swift
@@ -12,14 +12,14 @@ public class SignalingManager: BaseServiceManager {
         self["signalingReject"] = signalingReject
         self["signalingCancel"] = signalingCancel
         self["signalingHungUp"] = signalingHungUp
-        self["signalingGetRoomByGroupID"] = signalingGetRoomByGroupID
-        self["signalingGetTokenByRoomID"] = signalingGetTokenByRoomID
-        self["signalingUpdateMeetingInfo"] = signalingUpdateMeetingInfo
-        self["signalingCreateMeeting"] = signalingCreateMeeting
-        self["signalingJoinMeeting"] = signalingJoinMeeting
-        self["signalingOperateStream"] = signalingOperateStream
-        self["signalingGetMeetings"] = signalingGetMeetings
-        self["signalingCloseRoom"] = signalingCloseRoom
+//        self["signalingGetRoomByGroupID"] = signalingGetRoomByGroupID
+//        self["signalingGetTokenByRoomID"] = signalingGetTokenByRoomID
+//        self["signalingUpdateMeetingInfo"] = signalingUpdateMeetingInfo
+//        self["signalingCreateMeeting"] = signalingCreateMeeting
+//        self["signalingJoinMeeting"] = signalingJoinMeeting
+//        self["signalingOperateStream"] = signalingOperateStream
+//        self["signalingGetMeetings"] = signalingGetMeetings
+//        self["signalingCloseRoom"] = signalingCloseRoom
     }
 
     func setSignalingListener(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
@@ -51,37 +51,37 @@ public class SignalingManager: BaseServiceManager {
         Open_im_sdkSignalingHungUp(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[jsonString: "signalingInfo"])
     }
     
-    func signalingGetRoomByGroupID(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingGetRoomByGroupID(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "groupID"])
-    }
-    
-    func signalingGetTokenByRoomID(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingGetTokenByRoomID(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "roomID"])
-    }
-    
-    func signalingUpdateMeetingInfo(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingUpdateMeetingInfo(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[jsonString: "info"])
-    }
-    
-    func signalingCreateMeeting(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingCreateMeeting(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[jsonString: "info"])
-    }
-    
-    func signalingJoinMeeting(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingJoinMeeting(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[jsonString: "info"])
-    }
-    
-    func signalingOperateStream(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingOperateStream(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "streamType"], methodCall[string: "roomID"], methodCall[string: "userID"], methodCall[bool: "mute"], methodCall[bool: "muteAll"])
-    }
-    
-    func signalingGetMeetings(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingGetMeetings(BaseCallback(result: result), methodCall[string: "operationID"])
-    }
-    
-    func signalingCloseRoom(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
-        Open_im_sdkSignalingCloseRoom(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "roomID"])
-    }
+//    func signalingGetRoomByGroupID(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingGetRoomByGroupID(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "groupID"])
+//    }
+//
+//    func signalingGetTokenByRoomID(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingGetTokenByRoomID(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "roomID"])
+//    }
+//
+//    func signalingUpdateMeetingInfo(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingUpdateMeetingInfo(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[jsonString: "info"])
+//    }
+//
+//    func signalingCreateMeeting(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingCreateMeeting(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[jsonString: "info"])
+//    }
+//
+//    func signalingJoinMeeting(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingJoinMeeting(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[jsonString: "info"])
+//    }
+//
+//    func signalingOperateStream(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingOperateStream(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "streamType"], methodCall[string: "roomID"], methodCall[string: "userID"], methodCall[bool: "mute"], methodCall[bool: "muteAll"])
+//    }
+//
+//    func signalingGetMeetings(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingGetMeetings(BaseCallback(result: result), methodCall[string: "operationID"])
+//    }
+//
+//    func signalingCloseRoom(methodCall: FlutterMethodCall, result: @escaping FlutterResult){
+//        Open_im_sdkSignalingCloseRoom(BaseCallback(result: result), methodCall[string: "operationID"], methodCall[string: "roomID"])
+//    }
 }
 public class SignalingListener: NSObject, Open_im_sdk_callbackOnSignalingListenerProtocol {
   
@@ -123,16 +123,16 @@ public class SignalingListener: NSObject, Open_im_sdk_callbackOnSignalingListene
         CommonUtil.emitEvent(channel: channel, method: "signalingListener", type: "onHangUp", errCode: nil, errMsg: nil, data: s)
     }
     
-    public func onRoomParticipantConnected(_ s: String?) {
-        CommonUtil.emitEvent(channel: channel, method: "signalingListener", type: "onRoomParticipantConnected", errCode: nil, errMsg: nil, data: s)
-    }
-    
-    public func onRoomParticipantDisconnected(_ s: String?) {
-        CommonUtil.emitEvent(channel: channel, method: "signalingListener", type: "onRoomParticipantDisconnected", errCode: nil, errMsg: nil, data: s)
-    }
-    
-    public func onStreamChange(_ s: String?) {
-        CommonUtil.emitEvent(channel: channel, method: "signalingListener", type: "onStreamChange", errCode: nil, errMsg: nil, data: s)
-    }
+//    public func onRoomParticipantConnected(_ s: String?) {
+//        CommonUtil.emitEvent(channel: channel, method: "signalingListener", type: "onRoomParticipantConnected", errCode: nil, errMsg: nil, data: s)
+//    }
+//
+//    public func onRoomParticipantDisconnected(_ s: String?) {
+//        CommonUtil.emitEvent(channel: channel, method: "signalingListener", type: "onRoomParticipantDisconnected", errCode: nil, errMsg: nil, data: s)
+//    }
+//
+//    public func onStreamChange(_ s: String?) {
+//        CommonUtil.emitEvent(channel: channel, method: "signalingListener", type: "onStreamChange", errCode: nil, errMsg: nil, data: s)
+//    }
     
 }
